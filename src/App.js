d4d5886e99d54a812455a59121a5b28c3ed35ba6
Inigo Meshoulam <ijmeshoulam@gmail.com>
2021-07-05 19:21:51 +0100

    Markdown interpreter added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { useState } from 'react';
+
+import marked from 'marked';
+
 import './App.css';
 
-// Rendred by Editor. This displays the text.
-// TODO: This will run the markdown interpreter.
+
+// Rendred by Editor. This runs a markdown interpreter and displays text.
 const Viewer = (props) => {
+
+    const getMarkdownText = () => {
+        let rawMarkup = marked(props.text, {sanitize: true});
+        return { __html: rawMarkup };
+    }
 
     return (
         <>
-        <p>{props.text}</p>
+        <div id="preview" dangerouslySetInnerHTML={getMarkdownText()} />
         </>
     )
 }
@@ -16,7 +24,7 @@ const Viewer = (props) => {
 // Child component: Viewer which inherits the input text.
 const Editor = () => {
 
-    const [inputText, changeInputText] = useState("Hello!");
+    const [inputText, changeInputText] = useState(defaultText);
 
 
     const handleChange = (event) => {
@@ -44,5 +52,50 @@ const App = () => {
         </>
     )
 }
+
+const defaultText = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://en.wikipedia.org/wiki/Main_Page), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![Kitten](https://www.petmd.com/sites/default/files/styles/article_image/public/small-kitten-walking-towards_127900829_0.jpg?itok=ah_gTtbS)
+`;
 
 export default App;
